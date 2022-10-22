35018c483099fa9edd56e50c2058610085792ba0
Felipe Belisario <felipesantos.dsb@hotmail.com>
2022-10-22 13:11:37 -0300

    update files

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,49 @@
-const output = (excuses) => {
-    excuses.forEach((excuseSelected) => {
-        let id = document.createElement("id");
-
-        let excuse = document.createElement("excuse");
-
-        let category = document.createElement("category");
+;
 
 
-    });
+
+// GET JOKES
+
+const btnNext = document.querySelector('#btnNext');
+const sortedJoke = document.querySelector('#output');
+
+function getJokes() {
+  fetch('https://api.chucknorris.io/jokes/random')
+  .then(r => r.json())
+  .then(joke => {
+    sortedJoke.innerText = joke.value;
+  })
 }
 
+btnNext.addEventListener('click', getJokes);
 
 
-const getRandomExcuses = async () => {
-    const response = await fetch ("https://excuser.herokuapp.com/v1/excuse"
 
-    );
-    randomExcuses = await response.json();
-}
+
+
+   // COPY TO CLIPBOARD
+// const btnCopyToClipboard = document.querySelector('#copyToClipboard');
+//    function copyToClipboard() {
+    
+//     let copyText = document.querySelector("#output");
+  
+//     // Select the text field
+//     copyText.select();
+//     copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+//     // Copy the text inside the text field
+//     navigator.clipboard.writeText(copyText.value);
+    
+   
+// }
+
+
+let text = document.getElementById('output').innerHTML;
+  const copyContent = async () => {
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  }
